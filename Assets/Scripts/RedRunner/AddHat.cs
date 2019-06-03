6323d8d4b845ab6f225ad2c92cdee111ed1f85cd
Unknown <hugodekruijf@hotmail.com>
2019-06-03 13:30:05 +0200

    Decorator

---
+++ Assets/Scripts/RedRunner/AddHat.cs
@@ -0,0 +1,73 @@
+﻿using RedRunner.Collectables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddHat : Collectable
+{
+    public bool isJumphat;
+    public bool isSpeedHat;
+
+    BaseHead head;
+    JumpHat jumpHat;
+    SpeedHat speedHat;
+
+
+    public override SpriteRenderer SpriteRenderer
+    {
+        get
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public override Collider2D Collider2D => throw new System.NotImplementedException();
+
+    public override Animator Animator => throw new System.NotImplementedException();
+
+    public override bool UseOnTriggerEnter2D { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public override void Collect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (isJumphat && !isSpeedHat)
+        {
+            jumpHat.Use();
+            HatAttached();
+        }
+        if (isSpeedHat && !isJumphat)
+        {
+            speedHat.Use();
+            HatAttached();
+        }
+    }
+
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+    }
+
+    void Start()
+    {
+        head = new RedRunnerHead();
+        if (isJumphat && !isSpeedHat)
+        {
+            jumpHat = new JumpHat(head, 5f);
+        }
+        if (isSpeedHat && !isJumphat)
+        {
+            speedHat = new SpeedHat(head, 2f);
+        }
+
+    }
+
+    void HatAttached()
+    {
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        gameObject.transform.position = GameObject.FindGameObjectWithTag("Head").transform.position;
+        gameObject.transform.parent = GameObject.FindGameObjectWithTag("Head").transform;
+    }
+}
---
+++ Assets/Scripts/RedRunner/RedRunnerHead.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedRunnerHead : BaseHead
+{
+    public override void Use()
+    {
+        Debug.Log("bald");
+    }
+}
